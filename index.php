4f2bbdb1e43d6d15fe975396629ec7185c7aae24
jhsmu <91585216+jhsmu@users.noreply.github.com>
2023-10-03 12:53:46 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>login</title>
+</head>
+<body>
+    <div class="box">
+    <div class="form">
+        <form action="">
+            <h2>Incio de sesion</h2>
+            <div class="inputBox">
+                <input type="text" required>
+                <span>Usuario</span>
+                <i></i>
+            </div>
+            <div class="inputBox">
+                <input type="password" required>
+                <span>Contraseña</span>
+                <i></i>
+            </div>
+            
+        </form>
+        <input type="submit" value="Iniciar">
+    </div>
+    </div>
+    
+</body>
+</html>
