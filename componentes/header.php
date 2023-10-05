8eba42f126c96ff2611fdfe50437a3f4efbbdcef
jhsmu <91585216+jhsmu@users.noreply.github.com>
2023-10-04 19:30:28 -0500

    Add files via upload

---
+++ componentes/header.php
@@ -0,0 +1,43 @@
+<header class="vh-100 over-flow-hidden">
+    <!-- navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-transparent">
+  <div class="container">
+    <a class="navbar-brand" href="#">IchoClimaOnline</a>
+    <button class="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="sidebar offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header text-white border-bottom">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">IchoClimaOnline</h5>
+        <button type="button" class="btn-close btn-close-white shadow-none" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-center align-items-center fs-5 flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Nosotros</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Estadisticas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Informe</a>
+          </li>
+        </ul>
+        <!-- sign up/ login -->
+        <div class="d-flex flex-column flex-lg-row justify-content-center align-items-center gap-3">
+            <a href="" class="text-white text-decoration-none">Inicio Sesion</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+<main>
+        <section class="w-100 vh-100 d-flex flex-column justify-content-center align-items-center text-white fs-1">
+                <h1 style="font-size: 1.5em">IchoClimaOnline</h1>
+                <h2 style="font-size: 1.3em">Venta de paneles solares </h2>
+        </section>
+    </main>
+</header>
---
+++ principal.php
@@ -0,0 +1,28 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- css bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <!-- iconos en fontawesome -->
+    <script src="https://kit.fontawesome.com/4b93f520b2.js" crossorigin="anonymous"></script>
+    <!-- css footer y el header -->
+    <link rel="stylesheet" href="./css/footer-header.css">
+    <!-- css cuerpo -->
+    <link rel="stylesheet" href="./css/style_cuerpo.css">
+    <link rel="stylesheet" href="./css/style.css">
+      <link rel="icon" type="image/x-icon" href="./img/logo/icono.png">
+    <title>IchoClimaOnline</title>
+</head>
+<body>
+    <?php
+        include ('./componentes/header.php');
+    ?>
+    
+</body>
+</html>
